8baa86ced97bffa83ed9543b083cef869d8f607a
ashsajal1 <ashsajal1@gmail.com>
2023-12-10 18:39:17 +0800

    Added a markdown editor

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -1,12 +1,14 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 export default function NewPostPage() {
   return (
     <div className="space-y-3 max-w-xl">
         <Input placeholder="Enter the title"/>
-        <Textarea placeholder="Enter the blog"/>
+        <SimpleMDE placeholder="Enter the blog"/>
         <Button>Submit</Button>
     </div>
   )
