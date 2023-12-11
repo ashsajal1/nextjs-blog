aa055c2c472d3891da198091db0078b818a7d0fd
ashsajal1 <ashsajal1@gmail.com>
2023-12-11 11:11:49 +0800

    Handle errors

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -3,15 +3,15 @@ import { z } from "zod";
 import prisma from "@/lib/prisma";
 
 const postSchema = z.object({
-  title: z.string().min(2).max(100),
-  content: z.string().min(12).max(1000),
+  title: z.string().min(2, "Title is required.").max(100),
+  content: z.string().min(12, "Descripton is required.").max(1000),
 });
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const validation = postSchema.safeParse(body);
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
   }
 
   const { title, content } = body;
--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -7,6 +7,16 @@ import { useForm, Controller } from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from "next/navigation";
 
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
+
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
+import { useState } from "react";
+
+
 interface PostForm {
     title: string,
     content: string,
@@ -15,20 +25,37 @@ interface PostForm {
 export default function NewPostPage() {
     const router = useRouter()
     const { register, control, handleSubmit } = useForm<PostForm>();
+
+    const [error, setError] = useState('')
     // console.log(register('title'))
 
     return (
-        <form
-            className="space-y-3 max-w-xl"
-            onSubmit={handleSubmit(async(data) => {
-                console.log(data)
-                await axios.post('/api/post', data);
-                router.push('/post')
-            })}>
-            <Input placeholder="Enter the title" {...register('title')} />
-            <Controller name="content" control={control} render={({ field }) => <SimpleMDE placeholder="Enter the blog" {...field} />} />
+        <div className="max-w-xl">
+            {error && <Alert variant="destructive" className="mb-5">
+                <ExclamationTriangleIcon className="h-4 w-4" />
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                    {error}
+                </AlertDescription>
+            </Alert>}
+            <form
+                className="space-y-3"
+                onSubmit={handleSubmit(async (data) => {
+                    // console.log(data)
+                    setError("An error occurred!")
+                    try {
+                        await axios.post('/api/post', data);
+                        router.push('/post')
+                    } catch (error) {
+                        console.log(error)
+                    }
+                })}>
+                <Input placeholder="Enter the title" {...register('title')} />
+                <Controller name="content" control={control} render={({ field }) => <SimpleMDE placeholder="Enter the blog" {...field} />} />
 
-            <Button>Submit</Button>
-        </form>
+                <Button>Submit</Button>
+            </form>
+        </div>
+
     )
 }
